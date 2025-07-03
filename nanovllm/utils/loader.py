4d9f4099883444b39ad4e4cfd9c13131fc004def
kunato <kunato36@gmail.com>
2025-07-03 00:38:15 -0700

    raise error for if no weight is loaded

--- nanovllm/utils/loader.py
+++ nanovllm/utils/loader.py
@@ -10,6 +10,9 @@ def default_weight_loader(param: nn.Parameter, loaded_weight: torch.Tensor):
 
 
 def load_model(model: nn.Module, path: str):
+    has_safetensors = glob(os.path.join(path, "*.safetensors"))
+    if not has_safetensors:
+        raise ValueError(f"No safetensors found in {path}")
     packed_modules_mapping = getattr(model, "packed_modules_mapping", {})
     for file in glob(os.path.join(path, "*.safetensors")):
         with safe_open(file, "pt", "cpu") as f:
